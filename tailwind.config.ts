10faebf330cc1ad16b7fbb62cca3d78018158044
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-09-02 18:28:08 +0300

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "auth-background": "#c8c8c8",
+      },
     },
   },
   plugins: [],
