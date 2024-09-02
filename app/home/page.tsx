94f2a1e1c67418855805ea3795e86fccc7373517
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-09-02 20:50:53 +0300

    replce methods from router.push into router.replace

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "../components/layout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Home</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
