4630771058f6d1e01e98c65cb74cff23ad8cf6b2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-10-16 19:43:58 +0300

    create a marker

--- app/components/PlannerDesc.tsx
+++ app/components/PlannerDesc.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import CreateBlock from "./function/CreateBlock";
 
+import Draggable from "react-draggable";
+
 interface BlockPosition {
   x: number;
   y: number;
@@ -25,13 +27,30 @@ const PlannerDesc = () => {
     <div
       className="w-full h-full relative overflow-hidden"
       onClick={createAPlan}
+      onMouseMove={(e) => console.log(e)}
     >
-      smth
+      {/* smth*/}
       {positions.length > 0
         ? positions.map((position, index) => (
             <CreateBlock x={position?.x} y={position?.y} key={index} />
           ))
         : ""}
+      {/* <Draggable
+        axis="x"
+        handle=".handle"
+        defaultPosition={{ x: 0, y: 0 }}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}
+      >
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable> */}
       {/* <div
         className="bg-red-600 w-[226px] h-[250px] absolute transform translate-x-[-85%] translate-y-[-58%]"
         style={{ top: Y, left: X }}
--- app/components/function/CreateBlock.tsx
+++ app/components/function/CreateBlock.tsx
@@ -6,13 +6,25 @@ interface CreateBlockProps {
 }
 
 const CreateBlock = ({ x, y }: CreateBlockProps) => {
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    console.log({ x, y });
+  };
+
   return (
     <>
       {x !== undefined || y !== undefined ? (
         <div
           className="bg-red-600 w-[226px] h-[250px] absolute transform translate-x-[-85%] translate-y-[-58%]"
           style={{ top: y, left: x }}
-        ></div>
+        >
+          <div
+            className=" w-full absolute h-3"
+            onDragCapture={(e) => console.log(e)}
+          ></div>
+        </div>
       ) : (
         ""
       )}
