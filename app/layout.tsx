05d44809fc142307df6d36ca1504a885794c8024
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-10-25 23:12:45 +0300

    remove y-scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={(inter.className, "overflow-y-hidden")}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
