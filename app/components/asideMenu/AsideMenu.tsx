2ce3f91e9b075b34a5da9bfcdf6ab664ab0ba783
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-10-25 23:12:23 +0300

    change aside menu

--- app/components/asideMenu/AsideMenu.tsx
+++ app/components/asideMenu/AsideMenu.tsx
@@ -1,30 +1,44 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { FaHome, FaUser } from "react-icons/fa";
+import LogOutButton from "./LogOutButton";
 
 const AsideMenu = () => {
+  const router = useRouter();
+
   return (
     <aside className="bg-black text-white w-20 flex flex-col items-center h-[100vh]">
       <ul className="mt-8 flex flex-col gap-5">
         <li>
           <button
             className="border-2 w-14 h-14 rounded-full flex justify-center items-center border-white bg-white bg-opacity-20 p-3 transition-all hover:bg-opacity-100"
-            onClick={() => console.log("🏠")}
+            onClick={() => {
+              router.replace("/home");
+              console.log("🏠");
+            }}
           >
-            🏠
+            <FaHome />
           </button>
         </li>
         <li>
           <button
             className="border-2 w-14 h-14 rounded-full flex justify-center items-center border-white bg-white bg-opacity-20 p-3 transition-all hover:bg-opacity-100"
-            onClick={() => console.log("👳🏻‍♂️")}
+            onClick={() => {
+              router.replace("/me");
+              console.log("👳🏻‍♂️");
+            }}
           >
-            👳🏻‍♂️
+            <FaUser />
           </button>
         </li>
       </ul>
-      <button className="mt-auto mb-10 " onClick={() => console.log("exit")}>
-        🚶🏻‍♀️
-      </button>
+      {/* <button className="mt-auto mb-10" onClick={() => console.log("exit")}>
+        <IoMdExit />
+      </button> */}
+      <div className="mt-auto mb-10">
+        <LogOutButton />
+      </div>
     </aside>
   );
 };
