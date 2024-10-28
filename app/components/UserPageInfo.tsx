5ebad7973afefb25230c7c4a747f83409529efd6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-10-28 22:13:43 +0200

    if click change info open form and if click change password - redirect to page forgotPassword

--- app/components/UserPageInfo.tsx
+++ app/components/UserPageInfo.tsx
@@ -2,38 +2,95 @@
 
 import {
   selectUserEmail,
+  selectUserID,
   selectUserUsername,
 } from "@/redux/features/auth/selectors";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import FormInput from "./FormInput/FormInput";
+import { useAppDispatch } from "@/redux/hooks";
+import { refreshUser, updateAcc } from "@/redux/features/auth/auth-operations";
 
 const UserPageInfo = () => {
   const route = useRouter();
 
   const username = useSelector(selectUserUsername);
   const email = useSelector(selectUserEmail);
+  const userId = useSelector(selectUserID);
+
+  console.log("userId: ", userId);
+
+  const [changeInfoMode, setChangeInfoMode] = useState(false);
+  const [newUsername, setNewUsername] = useState(username as string);
+  const [newEmail, setNewEmail] = useState(email as string);
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   return (
     <div className="flex justify-center h-full">
       <div className="flex flex-col items-center my-auto justify-around h-[60%]">
         <FaRegUserCircle size={128} />
-        <h2>
-          <b>Name:</b> {username}
-        </h2>
-        <h3>
-          <b>Email:</b> {email}
-        </h3>
+        {changeInfoMode ? (
+          <>
+            <h2>
+              <b>Name:</b>
+              <FormInput
+                type="text"
+                value={newUsername}
+                onChange={(e) => setNewUsername(e.target.value)}
+              />
+            </h2>
+            <h3>
+              <b>Email:</b>
+              <FormInput
+                type="text"
+                value={newEmail}
+                onChange={(e) => setNewEmail(e.target.value)}
+              />
+            </h3>
+            <button
+              onClick={() => {
+                setChangeInfoMode(false);
+                console.log("Info updated", { newUsername, newEmail });
+                dispatch(
+                  updateAcc({
+                    id: userId,
+                    UpdatedUserData: {
+                      email: newEmail,
+                      username: newUsername,
+                    },
+                  })
+                );
+              }}
+            >
+              Save changes
+            </button>
+          </>
+        ) : (
+          <>
+            <h2>
+              <b>Name:</b> {username}
+            </h2>
+            <h3>
+              <b>Email:</b> {email}
+            </h3>
+            <button
+              onClick={() => {
+                setChangeInfoMode(true);
+              }}
+            >
+              Update info
+            </button>
+          </>
+        )}
         <button
           onClick={() => {
-            console.log("update Info");
-          }}
-        >
-          Update info
-        </button>
-        <button
-          onClick={() => {
+            route.replace("/authorization/forgotPassword");
             console.log("Change password");
           }}
         >
