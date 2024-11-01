037d55f30bdeff6fbf9ecd671c47bdddbe9b8861
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-01 19:47:52 +0200

    fullfilled createMarketForm

--- app/components/Forms/CreateMarkerForm.tsx
+++ app/components/Forms/CreateMarkerForm.tsx
@@ -1,27 +1,55 @@
 "use client";
 
+import { addTask } from "@/redux/features/MarkersPlan/marker-operations";
+import { useAppDispatch } from "@/redux/hooks";
 import { Formik, Field, Form, FormikHelpers } from "formik";
+import { useState } from "react";
 
 interface Values {
-  firstName: string;
-  lastName: string;
-  email: string;
+  titleOfMarker: string;
+  textOfTask: string;
 }
 
-export const CreateMarkerForm = ({ onClose }: { onClose: () => void }) => {
+type Prop = {
+  onClose: () => void;
+  position: {
+    xPos: number;
+    yPos: number;
+  };
+};
+
+export const CreateMarkerForm = ({ onClose, position }: Prop) => {
+  const dispatch = useAppDispatch();
+
+  const handleSubmitingForm = async (values: Values) => {
+    console.log({
+      username: values.titleOfMarker,
+      textOfTasks: values.textOfTask,
+      date: localStorage.getItem("date"),
+      x: position.xPos,
+      y: position.yPos,
+    });
+    const addTaskValues = {
+      title: values.titleOfMarker,
+      taskText: values.textOfTask,
+      date: localStorage.getItem("date"),
+    };
+    await dispatch(addTask(addTaskValues));
+    onClose();
+  };
+
   return (
     <div
       className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-40"
       onClick={(e) => {
         onClose();
         e.stopPropagation();
-      }} // Prevents click events inside the modal from closing it
+      }}
     >
       <div
         className="bg-white p-4 rounded-lg shadow-lg"
-        onClick={(e) => e.stopPropagation()} // Prevents click events inside the modal from closing it
+        onClick={(e) => e.stopPropagation()}
       >
-        <h1>Signup</h1>
         <button
           onClick={(e) => {
             onClose();
@@ -33,36 +61,39 @@ export const CreateMarkerForm = ({ onClose }: { onClose: () => void }) => {
         </button>
         <Formik
           initialValues={{
-            firstName: "",
-            lastName: "",
-            email: "",
+            titleOfMarker: "",
+            textOfTask: "",
           }}
           onSubmit={(
             values: Values,
             { setSubmitting }: FormikHelpers<Values>
           ) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-              onClose(); // Close the modal after submission
-            }, 500);
+            handleSubmitingForm(values);
+            setSubmitting(false);
           }}
         >
           <Form className="flex flex-col gap-4">
-            <label htmlFor="firstName">First Name</label>
-            <Field id="firstName" name="firstName" placeholder="John" />
+            <div className="flex gap-2">
+              <label htmlFor="titleOfMarker">
+                <b>Title</b>
+              </label>
+              <Field
+                id="titleOfMarker"
+                name="titleOfMarker"
+                placeholder="Title..."
+              />
+            </div>
 
-            <label htmlFor="lastName">Last Name</label>
-            <Field id="lastName" name="lastName" placeholder="Doe" />
-
-            <label htmlFor="email">Email</label>
-            <Field
-              id="email"
-              name="email"
-              placeholder="john@acme.com"
-              type="email"
-            />
-
+            <div className="flex gap-2">
+              <label htmlFor="textOfTask">
+                <b>Task`s text</b>
+              </label>
+              <Field
+                id="textOfTask"
+                name="textOfTask"
+                placeholder="Today I should..."
+              />
+            </div>
             <button
               type="submit"
               className="bg-blue-500 text-white py-2 px-4 rounded"
