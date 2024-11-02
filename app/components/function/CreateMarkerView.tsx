e57105d03573adab8dfbb465b5c3a60eed196881
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-02 23:57:35 +0200

    fix visual and add styles for <p>

--- app/components/function/CreateMarkerView.tsx
+++ app/components/function/CreateMarkerView.tsx
@@ -2,18 +2,23 @@ export interface MarkerProps {
   id: String;
   title: String;
   taskText: String;
+  // coordinates: {
+  //   x: number;
+  //   y: number;
+  // };
   x: number;
   y: number;
 }
 
 const CreateMarkerView = ({ title, taskText, x, y }: MarkerProps) => {
+  console.log({ title, taskText, x, y });
   return (
     <div
-      className="w-[226px] h-min-[250px] bg-yellow-200 flex flex-col p-2 absolute transform translate-x-[-85%] translate-y-[-80%]"
+      className="w-[226px] h-min-[250px] bg-yellow-200 flex flex-col p-2 absolute transform translate-x-[-85%] -translate-y-[150px]"
       style={{ top: y, left: x }}
     >
       <h3>{title}</h3>
-      <p>{taskText}</p>
+      <p className="text-wrap break-words hyphens-auto">{taskText}</p>
     </div>
   );
 };
