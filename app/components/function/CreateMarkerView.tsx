280b78959a7a76f1b335372f5b67bdfafb601c50
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-04 19:36:38 +0200

    changed markers view for placing and adding buttons to them

--- app/components/function/CreateMarkerView.tsx
+++ app/components/function/CreateMarkerView.tsx
@@ -1,3 +1,30 @@
+// export interface MarkerProps {
+//   id: String;
+//   title: String;
+//   taskText: String;
+//   // coordinates: {
+//   //   x: number;
+//   //   y: number;
+//   // };
+//   x: number;
+//   y: number;
+// }
+
+// const CreateMarkerView = ({ title, taskText, x, y }: MarkerProps) => {
+//   console.log({ title, taskText, x, y });
+//   return (
+//     <div
+//       className="w-[226px] h-min-[250px] bg-yellow-200 flex flex-col p-2 absolute transform translate-x-[-85%] -translate-y-[150px]"
+//       // style={{ top: y, left: x }}
+//     >
+//       <h3>{title}</h3>
+//       <p className="text-wrap break-words hyphens-auto">{taskText}</p>
+//     </div>
+//   );
+// };
+
+// export default CreateMarkerView;
+
 export interface MarkerProps {
   id: String;
   title: String;
@@ -6,16 +33,14 @@ export interface MarkerProps {
   //   x: number;
   //   y: number;
   // };
-  x: number;
-  y: number;
 }
 
-const CreateMarkerView = ({ title, taskText, x, y }: MarkerProps) => {
-  console.log({ title, taskText, x, y });
+const CreateMarkerView = ({ title, taskText }: MarkerProps) => {
+  console.log({ title, taskText });
   return (
     <div
-      className="w-[226px] h-min-[250px] bg-yellow-200 flex flex-col p-2 absolute transform translate-x-[-85%] -translate-y-[150px]"
-      style={{ top: y, left: x }}
+      className="w-[226px] h-min-[250px] bg-yellow-200 flex flex-col p-2 "
+      // style={{ top: y, left: x }}
     >
       <h3>{title}</h3>
       <p className="text-wrap break-words hyphens-auto">{taskText}</p>
