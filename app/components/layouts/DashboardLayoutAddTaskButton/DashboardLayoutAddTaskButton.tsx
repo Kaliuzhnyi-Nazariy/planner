76b6099f989a41ec7b9644342e447b550a8e484e
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-05 18:41:23 +0200

    randomize x and y during add button

--- app/components/layouts/DashboardLayoutAddTaskButton/DashboardLayoutAddTaskButton.tsx
+++ app/components/layouts/DashboardLayoutAddTaskButton/DashboardLayoutAddTaskButton.tsx
@@ -1,12 +1,31 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CreateMarkerForm } from "../../Forms/CreateMarkerForm";
 
 const DashboardLayoutAddTaskButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const [xPos, setXPos] = useState(0);
+  const [yPos, setYPos] = useState(0);
+
+  const xPosVal = () => {
+    const maxVal = window.innerWidth - 250;
+    return Math.floor(Math.random() * (maxVal - 300 + 1) + 300);
+    // console.log(finalRes);
+    // return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const yPosVal = () => {
+    const maxVal = window.innerHeight - 50;
+    return Math.floor(Math.random() * (maxVal - 80 + 1) + 80);
+    // console.log(finalRes);
+  };
+
   const handleOpen = () => {
     setIsModalOpen(true);
+
+    setXPos(xPosVal());
+    setYPos(yPosVal());
   };
 
   const handleClose = () => {
@@ -24,7 +43,7 @@ const DashboardLayoutAddTaskButton = () => {
             e.stopPropagation();
           }}
         >
-          <CreateMarkerForm onClose={handleClose} />
+          <CreateMarkerForm onClose={handleClose} position={{ xPos, yPos }} />
         </div>
       ) : (
         ""
