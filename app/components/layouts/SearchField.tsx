42e84a9c0cbc5a498bc4b463c4ecbfc52c199e0f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-14 23:26:05 +0200

    improved logic by adding local storrage and synchronized by type of device

--- app/components/layouts/SearchField.tsx
+++ app/components/layouts/SearchField.tsx
@@ -2,16 +2,24 @@
 
 import { addFilter } from "@/redux/features/MarkersPlan/filter-slice";
 import { useAppDispatch } from "@/redux/hooks";
+import FormInput from "../FormInput/FormInput";
+import { useState } from "react";
 
 const SearchField = () => {
   const dispatch = useAppDispatch();
+  const [searchingQuery, setSearchingQuery] = useState(
+    localStorage.getItem("search") || ""
+  );
 
   return (
     <input
       type="text"
       className="border-b-2 border-b-orange-500"
       placeholder="Search..."
+      value={searchingQuery}
       onChange={(e) => {
+        setSearchingQuery(e.target.value);
+        localStorage.setItem("search", e.target.value);
         dispatch(addFilter(e.target.value));
       }}
     />
